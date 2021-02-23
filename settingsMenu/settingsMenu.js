2264842a7999a8e64f3964ce7d1222d603a8507e
Jana Němcová <ja_nemcova@volny.cz>
2021-02-23 12:01:40 +0100

    add redirect function

--- settingsMenu/settingsMenu.js
+++ settingsMenu/settingsMenu.js
@@ -6,10 +6,10 @@ input: array of dashboards to render
 import React from "react";
 import { Link } from 'react-router-dom';
 
- export const renderNavBarSettings = (dashboards) => {
+ export const renderNavBarSettings = (dashboards, redirect) => {
         var navBar = [];
         for (var i = 0; i < dashboards.length; i++) {
-                navBar.push(<Link to={"/" + dashboards[i]} className="bg-dark list-group-myitem list-group-item-action" onClick={this.redirect} key={dashboards[i]}>
+                navBar.push(<Link to={"/" + dashboards[i]} className="bg-dark list-group-myitem list-group-item-action" onClick={redirect()} key={dashboards[i]}>
                     <div className="d-flex w-100 justify-content-start align-items-center">
                         <img className="marginRight" src={require(`./icons/${dashboards[i]}.png`)} alt={dashboards[i]} />
                         <span className={this.state.activeDashboard === dashboards[i] ? "menu-collapsed menuText activeDashboard" : "menu-collapsed menuText"}>{this.capitalizeFirstLetter(dashboards[i])}</span>
