eee5cd3b734b26142e66f97ae4b1307076851165
Jana Němcová <ja_nemcova@volny.cz>
2021-10-22 21:11:34 +0200

    move style to module

--- index.js
+++ index.js
@@ -3,4 +3,10 @@ export { getFilters } from './src/filter/getFilters';
 export { renderNavBar } from './src/menu/menu.js';
 export { elasticsearchConnection } from './src/helpers/elasticsearchConnection.js';
 export { getProfile } from './src/helpers/getProfile.js';
-export { getGeoData, decryptGeoData } from './src/helpers/geomapData.js'
+export { getGeoData, decryptGeoData } from './src/helpers/geomapData.js';
+export { Colors } from './src/style/Colors.js';
+export { ColorsGreen } from './src/style/ColorsGreen.js';
+export { ColorsNoGreen } from './src/style/ColorsNoGreen.js';
+export { ColorsRedGreen } from './src/style/ColorsRedGreen.js';
+export { ColorsReds } from './src/style/ColorsReds.js';
+export { ColorType } from './src/style/ColorType.js';
