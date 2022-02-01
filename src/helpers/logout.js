11761ce485ea84dc5b19dadf467662e5c9cb8f24
Jana Němcová <ja_nemcova@volny.cz>
2022-02-01 14:08:48 +0100

    ldap logout

--- src/helpers/logout.js
+++ src/helpers/logout.js
@@ -1,3 +1,13 @@
+const deleteAllCookies = () => {
+  const cookies = document.cookie.split(";");
+
+  for (const cookie of cookies) {
+      const eqPos = cookie.indexOf("=");
+      const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+      document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+  }
+}
+
 /**
 * logout function - fake user login
 * @return {} nothing
@@ -14,6 +24,7 @@ export function logout() {
       }
     });
     window.location.reload();
+    deleteAllCookies();
   }
   catch (error) {
   }
