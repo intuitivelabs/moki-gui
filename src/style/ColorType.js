b54f7d8b49278a0f1d98157b26601c46a8fcb81c
Jana Nemcova <jana.nemcova@frafos.com>
2023-03-30 09:04:14 +0200

    new reg-lost exceeded events

--- src/style/ColorType.js
+++ src/style/ColorType.js
@@ -46,6 +46,7 @@ export const ColorType = {
   "CA_unreachable": "#2b2b29",
   "CA_reachable_again": "#000075",
   "poor_parallel_reg": "#7FA900",
-  "security_metrics": "#46f0f0"
+  "security_metrics": "#46f0f0",
+  'reg-lost': '#41FA51',
 };
 export default ColorType;
